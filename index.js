2731f65ef23da0f02a2d7e3735e2ff0be146ea84
Dragos-Paul Pop <froind@gmail.com>
2021-06-02 13:31:23 -0700

    favicon

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  console.log('req: ', req.method, req.url);
+
+  if (req.url === '/favicon.ico') {
+    var fileStream = fs.createReadStream("./favicon.ico");
+    return fileStream.pipe(res);
+  } else {
+    res.write('Hello World!'); //write a response to the client
+  }
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
 console.log('Server pornit pe http://localhost:8080');
