183a4f78bb4862b5f458e736df7f05bbf6beb6b2
Dragos-Paul Pop <froind@gmail.com>
2021-06-02 13:11:08 -0700

    simple node htttp server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('it works')
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+console.log('Server pornit pe http://localhost:8080');
