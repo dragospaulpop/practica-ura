6828de834ce9060f728688da20704d24d184bec2
Dragos-Paul Pop <froind@gmail.com>
2021-06-03 12:24:43 +0300

    credentials.txt solutionat

--- index.js
+++ index.js
@@ -29,17 +29,43 @@ app.get('/read', function (req, res) {
 
 app.post('/write', function (req, res) {
     const data = req.body.payload.replace(/, /g, '\n')
+    const credentials = `${req.body.username}:${req.body.password}`
 
     try {
-        const path = './data/leaderboard.txt'
-        fs.writeFileSync(path, data)
+        const path = './data/credentials.txt'
+        const contents = fs.readFileSync(path).toString()
+        const credentialsDB = contents.split(/\r?\n/)        
+        
+        const found = credentialsDB.find(item => item === credentials) 
+        
+        // let found = null
+        // for (let i = 0; i < credentialsDB.length; i++) {
+        //     if (credentialsDB[i] === credentials) {
+        //         found = true
+        //         break
+        //     }
+        // }
+
+        if (found) {
+            try {
+                const path = './data/leaderboard.txt'
+                fs.writeFileSync(path, data)
+                res.send('success!!!')
+            } catch (err) {
+                console.log('Eroare: ', err)
+                res.status(500)
+                res.send('Eroare!')
+            }
+        } else {
+            console.log('Invalid credentials!')
+            res.status(401)
+            res.send('Invalid credentials!')
+        }
     } catch (err) {
         console.log('Eroare: ', err)
         res.status(500)
         res.send('Eroare!')
-    }
-
-    res.send(`Am primit: ${data}`)
+    }       
 })
 
 app.use(express.static('public'))
